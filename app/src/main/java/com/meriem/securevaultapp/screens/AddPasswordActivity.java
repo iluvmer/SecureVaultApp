2b524c7a7bc7b05d21bcc56ced88e94097a40e12
khadidja <khadidjaslimani434@gmail.com>
2025-05-29 14:12:36 +0100

    added generator when setting a password as well as an eye to see the password

--- app/src/main/java/com/meriem/securevaultapp/screens/AddPasswordActivity.java
+++ app/src/main/java/com/meriem/securevaultapp/screens/AddPasswordActivity.java
@@ -1,10 +1,14 @@
 package com.meriem.securevaultapp.screens;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,7 +25,10 @@ public class AddPasswordActivity extends AppCompatActivity {
 
     EditText editTextWebsite, editTextEmail, editTextPassword;
     Button buttonSave;
+    boolean isPasswordVisible = false;
+    private static final int PASSWORD_GENERATOR_REQUEST = 100;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,5 +62,49 @@ public class AddPasswordActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        ImageButton buttonOpenGenerator = findViewById(R.id.buttonGenerator);
+        buttonOpenGenerator.setOnClickListener(v -> {
+            Intent intent = new Intent(this, PasswordGeneratorActivity.class);
+            startActivityForResult(intent, PASSWORD_GENERATOR_REQUEST);
+        });
+
+        editTextPassword.setOnTouchListener((v, event) -> {
+            final int DRAWABLE_END = 2;
+
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                if (event.getRawX() >= (editTextPassword.getRight() - editTextPassword.getCompoundDrawables()[DRAWABLE_END].getBounds().width())) {
+                    isPasswordVisible = !isPasswordVisible;
+
+
+                    if (isPasswordVisible) {
+                        editTextPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                        editTextPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.eye, 0);
+
+                    } else {
+                        editTextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        editTextPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.eyec, 0);
+
+                    }
+
+
+                    editTextPassword.setSelection(editTextPassword.length());
+                    v.performClick();
+                    return true;
+                }
+            }
+            return false;
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PASSWORD_GENERATOR_REQUEST && resultCode == RESULT_OK) {
+            String password = data.getStringExtra("generatedPassword");
+            editTextPassword.setText(password);
+        }
     }
 }
